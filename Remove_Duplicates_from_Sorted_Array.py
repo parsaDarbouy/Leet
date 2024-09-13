680ccfb10c251f2f8e9199c507cfafa5a02fa105
pd00 <parsadarbouy@gmail.com>
2024-09-13 14:49:49 -0600

    Remove Duplicates from Sorted Array from Top Interview 150

---
+++ Remove_Duplicates_from_Sorted_Array.py
@@ -0,0 +1,31 @@
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        pointer_1 = 0
+        pointer_2 = 0
+        temp_val = None
+        length = len(nums)
+        if length == 1:
+            return 1
+
+        while pointer_2 < length:
+            if temp_val is None:
+                temp_val = nums[pointer_1]
+                pointer_1 += 1
+                pointer_2 += 1
+                continue
+            if temp_val == nums[pointer_2]:
+                if temp_val == nums[pointer_2]:
+                    pointer_2 += 1
+                    continue
+                else:
+                    nums[pointer_1] = nums[pointer_2]
+                    temp_val = nums[pointer_1]
+                    pointer_1 += 1
+                    pointer_2 += 1
+            else:
+                nums[pointer_1] = nums[pointer_2]
+                temp_val = nums[pointer_1]
+                pointer_1 += 1
+                pointer_2 += 1
+
+        return pointer_1
