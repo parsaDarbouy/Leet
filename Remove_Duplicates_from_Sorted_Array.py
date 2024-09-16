9bb2d294871fa3ac848217e11b85cbcd8520e1a9
pd00 <parsadarbouy@gmail.com>
2024-09-16 14:59:58 -0600

    Removed a redundant code.

--- Remove_Duplicates_from_Sorted_Array.py
+++ Remove_Duplicates_from_Sorted_Array.py
@@ -6,7 +6,6 @@ class Solution:
         length = len(nums)
         if length == 1:
             return 1
-
         while pointer_2 < length:
             if temp_val is None:
                 temp_val = nums[pointer_1]
@@ -14,14 +13,7 @@ class Solution:
                 pointer_2 += 1
                 continue
             if temp_val == nums[pointer_2]:
-                if temp_val == nums[pointer_2]:
-                    pointer_2 += 1
-                    continue
-                else:
-                    nums[pointer_1] = nums[pointer_2]
-                    temp_val = nums[pointer_1]
-                    pointer_1 += 1
-                    pointer_2 += 1
+                pointer_2 += 1
             else:
                 nums[pointer_1] = nums[pointer_2]
                 temp_val = nums[pointer_1]
