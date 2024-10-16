e51dd2cddf9d1cab9554e91c38621a3bccd0f477
pd00 <parsadarbouy@gmail.com>
2024-10-16 12:37:44 -0600

    cleaner version for 21. Merge Two Sorted Lists

--- 21_Merge_Two_Sorted_Lists.py
+++ 21_Merge_Two_Sorted_Lists.py
@@ -5,24 +5,9 @@
 #         self.next = next
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        head = None
-        if list1 is None:
-            if list2 is None:
-                return None
-            else:
-                return list2
-
-        if list2 is None:
-            return list1
-
+        head = ListNode()
         pointer_1 = list1
         pointer_2 = list2
-        if pointer_1.val < pointer_2.val:
-            head = pointer_1
-            pointer_1 = pointer_1.next
-        else:
-            head = pointer_2
-            pointer_2 = pointer_2.next
 
         current_node = head
 
@@ -31,20 +16,12 @@ class Solution:
                 if pointer_2 is None:
                     break
                 else:
-                    print(pointer_2.val)
                     current_node.next = pointer_2
-                    pointer_2 = pointer_2.next
-                    current_node = current_node.next
-                    continue
-
+                    break
             if pointer_2 is None:
                 current_node.next = pointer_1
-                pointer_1 = pointer_1.next
-                current_node = current_node.next
-                continue
+                break
             
-            
-
             if pointer_1.val < pointer_2.val:
                 current_node.next = pointer_1
                 pointer_1 = pointer_1.next
@@ -53,6 +30,6 @@ class Solution:
                 pointer_2 = pointer_2.next
 
             current_node = current_node.next
-        return head
+        return head.next
 
         
